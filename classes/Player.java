c5d25418ceab276e3860c992494ec2e85cb85d4c
FilippoGarofalo <125985963+FilippoGarofalo@users.noreply.github.com>
2023-03-27 17:40:32 +0200

    Add files via upload

--- classes/Player.java
+++ classes/Player.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import java.util.Scanner
+
 public class Player {
     final private String name;
     private int sharedScore;
-    private int personalGoal;
+    private int personalScore;
     private AbstractPersonalGoal personalGoal;
 
-    private Bookshelf myShelfie;
+    private Bookshelf bookshelf;
 
     public Player(String playerName) {
         this.name = playerName;
@@ -22,8 +24,8 @@ public class Player {
         return this.sharedScore;
     }
 
-    public int getPersonalGoal(){
-        return this.personalGoal;
+    public int getPersonalScore(){
+        return this.personalScore;
     }
 
     public void updateSharedScore(int newPoints){
@@ -31,33 +33,54 @@ public class Player {
     }
 
     public void updatePersonalScore(int newPoints){
-        this.personalGoal += newPoints;
+        this.personalScore += newPoints;
     }
 
     //passes coordinates buffer to Game who will first check if them are
     //available and then eventually let him insert the correspondent cards
     //into his bookshelf
-    public  Coordinates[] chooseCards(){
-        Coordinates[] buffer= new Coordinates[3];
-        //choose cards from GUI/CLI cards and assign it
-        //to buffer
-        return buffer;
+    public Coordinates[] chooseCards(){
+        int x,y;
+        int exit;
+        Coordinates[] AUXcoor= new Coordinates[3];
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0 ; i<3 || exit==1; i++){
+            System.out.println("Inserire numero riga\n");
+            x = scanner.nextInt();
+            System.out.println("Inserire numero colonna\n");
+            y = scanner.nextInt();
+            AUXcoor[i] = new Coordinates(x,y);
+            System.out.println("1 per scegliere un'altra carta, 0 per confermare\n");
+            exit = scanner.nextInt();
+        }
+        return AUXcoor;
     }
 
     //player has no access to Board, Game will pass him the cards.
     //here we are already sure that this cards are available to be inserted
-    public void insertInBookshelf (Card[] buffer){
-        int column;
-        Coordinates AUXkey = new Coordinates();
-        //choose column from GUI/CLI
-        //reorder buffer as preferred
-        for(int i=0 ; i<buffer.length && buffer[i]!=null; i++){
-            //wait for bookshelf commit ...
+    public boolean insertInBookshelf (Buffer buffer){
+        int col , insertable;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digitare numero colonna in cui si vuole inserire le carte\n");
+        col = scanner.nextInt();
+        insertable = this.bookshelf.getInsertable(col);
+        if(buffer.getBuffer().length <= insertable) {
+            this.bookshelf.insert(buffer, col);
+            return true;
+        }else{
+            return false;
         }
     }
 
-    public void orderCards(Card[] buffer){
-        //discuss about GUI/CLI
+    public void orderCards(Buffer buffer){
+        int[] indexOrder = new int [buffer.getBuffer().length];
+        Scanner scanner = new Scanner(System.in);
+        bookshelf.print();
+        System.out.println(buffer);
+        for(int i=0 ; i<buffer.getBuffer().length ; i++){
+            indexOrder[i] = scanner.nextInt();
+        }
+        buffer.order(indexOrder);
     }
 
 }
