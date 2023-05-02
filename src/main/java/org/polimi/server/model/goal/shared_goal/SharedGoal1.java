470aa957f520095e1c529c880f4159f098208797
Filippo Garofalo <filippo.garofalo@mail.polimi.it>
2023-05-02 12:04:32 +0200

    Cahnged implemetation for Shared GOal 1, the previous one was wrong

--- src/main/java/org/polimi/server/model/goal/shared_goal/SharedGoal1.java
+++ src/main/java/org/polimi/server/model/goal/shared_goal/SharedGoal1.java
@@ -1,6 +1,11 @@
 package org.polimi.server.model.goal.shared_goal;
 
 import org.polimi.server.model.Card;
+import org.polimi.server.model.Coordinates;
+
+import java.util.ArrayList;
+
+import static org.polimi.GameRules.bookshelfRowColIsInBound;
 
 /**
  * concrete object that represents the 1st shared goal
@@ -18,12 +23,18 @@ public class SharedGoal1 extends AbstractSharedGoal {
     @Override
     protected boolean achieved(Card[][] tmpGrid) {
         int count=0;
-        for(int i=ROW-1 ; i>=1 ; i=i-2){
+        ArrayList<ArrayList<Coordinates>> list = new ArrayList<ArrayList<Coordinates>>();
+        ArrayList<Coordinates> result = new ArrayList<Coordinates>();
+        Coordinates coor;
+        for(int i=0 ; i<ROW ; i++){
             for(int j=0 ; j<COL ; j++){
-                if(tmpGrid[i][j]!=null && tmpGrid[i-1][j]!=null) {
-                    if (tmpGrid[i][j].getColor() == tmpGrid[i - 1][j].getColor()) {
-                        count++;
-                    }
+                coor = new Coordinates(i,j);
+                if (explore(tmpGrid, coor)!=null){
+                    result = explore(tmpGrid, coor);
+                }
+                if(distanceIsValid(tmpGrid, result, list)){
+                    list.add(result);
+                    count++;
                 }
             }
             if(count>=6){
@@ -31,6 +42,77 @@ public class SharedGoal1 extends AbstractSharedGoal {
             }
         }
         return false;
+    }
+
+    private ArrayList<Coordinates> explore (Card[][] grid, Coordinates startCoor){
+        if(grid[startCoor.getRow()][startCoor.getCol()]!=null){
+            Coordinates tmpCoor = new Coordinates(startCoor.getRow()-1, startCoor.getCol());
+            if(bookshelfRowColIsInBound(tmpCoor)){
+                if(grid[tmpCoor.getRow()][tmpCoor.getCol()]!=null) {
+                    if (grid[startCoor.getRow()][startCoor.getCol()].getColor() == grid[tmpCoor.getRow()][tmpCoor.getCol()].getColor()) {
+                        ArrayList<Coordinates> result = new ArrayList<Coordinates>();
+                        result.add(startCoor);
+                        result.add(tmpCoor);
+                        return result;
+                    }
+                }
+            }
+            tmpCoor = new Coordinates(startCoor.getRow()+1, startCoor.getCol());
+            if(bookshelfRowColIsInBound(tmpCoor)){
+                if(grid[tmpCoor.getRow()][tmpCoor.getCol()]!=null) {
+                    if (grid[startCoor.getRow()][startCoor.getCol()].getColor() == grid[tmpCoor.getRow()][tmpCoor.getCol()].getColor()) {
+                        ArrayList<Coordinates> result = new ArrayList<Coordinates>();
+                        result.add(startCoor);
+                        result.add(tmpCoor);
+                        return result;
+                    }
+                }
+            }
+            tmpCoor = new Coordinates(startCoor.getRow(), startCoor.getCol()+1);
+            if(bookshelfRowColIsInBound(tmpCoor)){
+                if(grid[tmpCoor.getRow()][tmpCoor.getCol()]!=null) {
+                    if (grid[startCoor.getRow()][startCoor.getCol()].getColor() == grid[tmpCoor.getRow()][tmpCoor.getCol()].getColor()) {
+                        ArrayList<Coordinates> result = new ArrayList<Coordinates>();
+                        result.add(startCoor);
+                        result.add(tmpCoor);
+                        return result;
+                    }
+                }
+            }
+            tmpCoor = new Coordinates(startCoor.getRow(), startCoor.getCol()-1);
+            if(bookshelfRowColIsInBound(tmpCoor)){
+                if(grid[tmpCoor.getRow()][tmpCoor.getCol()]!=null) {
+                    if (grid[startCoor.getRow()][startCoor.getCol()].getColor() == grid[tmpCoor.getRow()][tmpCoor.getCol()].getColor()) {
+                        ArrayList<Coordinates> result = new ArrayList<Coordinates>();
+                        result.add(startCoor);
+                        result.add(tmpCoor);
+                        return result;
+                    }
+                }
+            }
+        }else{
+            return null;
+        }
+        return null;
+    }
+
+    private boolean distanceIsValid (Card[][] grid, ArrayList<Coordinates> result, ArrayList<ArrayList<Coordinates>> list){
+        for(int i=0; i<list.size() ; i++){
+            for(int j=0 ; j<2 ; j++){
+                if(     ((result.get(j).getRow()+1==list.get(i).get(0).getRow())&&(result.get(j).getCol()==list.get(i).get(0).getCol())) ||
+                        ((result.get(j).getRow()==list.get(i).get(0).getRow())&&(result.get(j).getCol()-1==list.get(i).get(0).getCol())) ||
+                        ((result.get(j).getRow()==list.get(i).get(0).getRow())&&(result.get(j).getCol()+1==list.get(i).get(0).getCol())) ||
+                        ((result.get(j).getRow()-1==list.get(i).get(0).getRow())&&(result.get(j).getCol()==list.get(i).get(0).getCol())) ||
+                        ((result.get(j).getRow()+1==list.get(i).get(1).getRow())&&(result.get(j).getCol()==list.get(i).get(1).getCol())) ||
+                        ((result.get(j).getRow()==list.get(i).get(1).getRow())&&(result.get(j).getCol()-1==list.get(i).get(1).getCol())) ||
+                        ((result.get(j).getRow()==list.get(i).get(1).getRow())&&(result.get(j).getCol()+1==list.get(i).get(1).getCol())) ||
+                        ((result.get(j).getRow()-1==list.get(i).get(1).getRow())&&(result.get(j).getCol()==list.get(i).get(1).getCol()))
+                ){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
