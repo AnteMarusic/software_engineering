e35700d9d9c08698b0a0516e5621e769967e3075
kala1221 <kalanakalpitha.kalupahana@mail.polimi.it>
2023-05-11 23:19:36 +0200

    new sharedgoal3 test, achievedTrue3 points out sharedgoal3 problem

--- src/test/java/org/polimi/server/model/goal/shared_goal/SharedGoal3Test.java
+++ src/test/java/org/polimi/server/model/goal/shared_goal/SharedGoal3Test.java
@@ -74,6 +74,36 @@ class SharedGoal3Test {
         assertEquals(4, goal.getScore(grid));
     }
     @Test
+    void achievedTrue3() {
+        Card[][] grid = new Card[6][5];
+        grid[0][0] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[0][1] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[0][2] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[0][3] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[0][4] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+
+        grid[1][0] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[1][1] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[1][2] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[1][3] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[1][4] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+
+
+        grid[2][0] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[2][1] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[2][3] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[2][4] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+
+        grid[3][0] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[3][1] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[3][3] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+        grid[3][4] = new Card(Card.Color.WHITE, Card.State.PICKABLE);
+
+        SharedGoal3 goal = new SharedGoal3(2);
+
+        assertEquals(4, goal.getScore(grid));
+    }
+    @Test
     void achievedFalse() {
         Card[][] grid = new Card[6][5];;
         SharedGoal3 goal = new SharedGoal3(2);
