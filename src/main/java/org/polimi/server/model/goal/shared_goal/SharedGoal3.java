67a0122d8e31fa18a1733e34d39098253ee22049
kala1221 <kalanakalpitha.kalupahana@mail.polimi.it>
2023-05-11 15:02:53 +0200

    sharedgoal 3

--- src/main/java/org/polimi/server/model/goal/shared_goal/SharedGoal3.java
+++ src/main/java/org/polimi/server/model/goal/shared_goal/SharedGoal3.java
@@ -45,12 +45,53 @@ public class SharedGoal3 extends AbstractSharedGoal{
                     if(checkFourinaRow(tmpGrid, coor, "column")){
                         banCoordinates(bannedCoordinates, coor, "column");
                         count++;
+                        System.out.println(coor + " column");
                     }
                 }
                 else if(j <= 1){
                     if(checkFourinaRow(tmpGrid, coor, "row")){
                         banCoordinates(bannedCoordinates, coor, "row");
                         count++;
+                        System.out.println(coor+" row");
+                    }
+                }
+                if(count >= 4)
+                    return true;
+            }
+        }
+
+        //second iteration, reading now rows from right to left
+        bannedCoordinates = new ArrayList<Coordinates>(21);
+        count = 0;
+        for(int i=0 ; i<ROW ; i++){
+            for(int j=COL-1 ; j>=0 ; j--) {
+                coor = new Coordinates(i,j);
+                if (!validCoordinates2(i, j) || bannedCoordinates.contains(coor))
+                    continue;
+                if(i <= 2 && j >= 3){
+                    if(checkRowRightToLeft(tmpGrid, coor)){
+                        banCoordinatesRowRightToLeft(bannedCoordinates, coor);
+                        count++;
+                        System.out.println(coor+" row 2");
+                    }
+                    else if(checkFourinaRow(tmpGrid, coor, "column")){
+                        banCoordinates(bannedCoordinates, coor, "column");
+                        count++;
+                        System.out.println(coor + " column 2");
+                    }
+                }
+                else if(i <= 2){
+                    if(checkFourinaRow(tmpGrid, coor, "column")){
+                        banCoordinates(bannedCoordinates, coor, "column");
+                        count++;
+                        System.out.println(coor + " column 2");
+                    }
+                }
+                else if(j <= 1){
+                    if(checkRowRightToLeft(tmpGrid, coor)){
+                        banCoordinatesRowRightToLeft(bannedCoordinates, coor);
+                        count++;
+                        System.out.println(coor+" row 2");
                     }
                 }
                 if(count >= 4)
@@ -80,6 +121,19 @@ public class SharedGoal3 extends AbstractSharedGoal{
         }
         return false;
     }
+    private boolean checkRowRightToLeft(Card[][] array, Coordinates startingCoords){
+        int row=startingCoords.getRow(), col= startingCoords.getCol();
+        if(array[row][col]!=null && array[row][col-1]!=null && array[row][col-2]!=null && array[row][col-3]!=null){
+            return array[row][col].getColor() == array[row][col-1].getColor() &&
+                    array[row][col].getColor() == array[row][col-2].getColor() &&
+                    array[row][col].getColor() == array[row][col-3].getColor();
+            }
+
+        return false;
+    }
+    private boolean validCoordinates2(int i, int j){
+        return i >= 0 && i <= 2 || j >= 3 && j <= 4;
+    }
     private void banCoordinates(ArrayList<Coordinates> bannedCoordinates, Coordinates startingCoords, String constantIndex){
         int row = startingCoords.getRow(), col= startingCoords.getCol();
         Coordinates coor1, coor2, coor3;
@@ -102,5 +156,17 @@ public class SharedGoal3 extends AbstractSharedGoal{
             bannedCoordinates.add(coor3);
         }
     }
+    private void banCoordinatesRowRightToLeft(ArrayList<Coordinates> bannedCoordinates, Coordinates startingCoords){
+        int row = startingCoords.getRow(), col= startingCoords.getCol();
+        Coordinates coor1, coor2, coor3;
+            coor1= new Coordinates(row, col-1);
+            coor2= new Coordinates(row, col-2);
+            coor3= new Coordinates(row, col-3);
+            bannedCoordinates.add(startingCoords);
+            bannedCoordinates.add(coor1);
+            bannedCoordinates.add(coor2);
+            bannedCoordinates.add(coor3);
+    }
+
 }
 
