c78dc72be083cddda6668ce87dbcc7e70ec9186b
AnteMarusic <antonio.marusic@mail.polimi.it>
2023-06-05 15:01:26 +0200

    improved client starter code

--- src/main/java/org/polimi/client/ClientStarter.java
+++ src/main/java/org/polimi/client/ClientStarter.java
@@ -17,27 +17,70 @@ import java.rmi.registry.Registry;
 import java.util.Scanner;
 
 public class ClientStarter {
+    private static final int rmiPort = 1099;
+    private static final int socketPort = 8181;
     public static void main(String[] args) throws IOException, NotBoundException {
         Scanner scanner = new Scanner(System.in);
         int input;
         boolean bool;
-        System.out.println("choose cli or gui");
-        System.out.println("(1) cli");
-        System.out.println("(2) gui");
-        input=scanner.nextInt();
-        if(input==1){
-            System.out.println("choose rmi or socket");
-            System.out.println("(1) rmi");
-            System.out.println("(2) scoket");
-            System.out.println("type 1 or 2");
-            input = scanner.nextInt();
-            if(input == 1){
-                RMIClient rmiClient = new RMIClient(1099);
-                do {
-                    bool = rmiClient.startConnection();
-                } while (!bool);
-                rmiClient.login();
-             /*while (rmiClient.ifConnected()) {
+        boolean firstWhile = true;
+        boolean secondWhile = true;
+        while (firstWhile) {
+            System.out.println("choose cli or gui");
+            System.out.println("(1) cli");
+            System.out.println("(2) gui");
+            if (scanner.hasNextInt()) {
+                // If the next input is an integer
+                input = scanner.nextInt();
+                if (input == 1) {
+                    firstWhile = false;
+                    while (secondWhile) {
+                        System.out.println("choose rmi or socket");
+                        System.out.println("(1) rmi");
+                        System.out.println("(2) socket");
+                        System.out.println("type 1 or 2");
+                        if (scanner.hasNextInt()) {
+                            input = scanner.nextInt();
+                            if (input == 1) {
+                                secondWhile = false;
+                                RMIClient rmiClient = new RMIClient(rmiPort);
+                                do {
+                                    bool = rmiClient.startConnection();
+                                } while (!bool);
+                                rmiClient.login();
+                            } else if (input == 2) {
+                                secondWhile = false;
+                                SocketClient socket = new SocketClient(socketPort);
+                            } else {
+                                System.out.println("Invalid input. please enter 1 or 2.");
+                                scanner.nextLine();
+                            }
+                        } else {
+                            // If the next input is not an integer
+                            System.out.println("Invalid input. Please enter an integer.");
+                            scanner.nextLine();
+                        }
+                    }
+                }
+                else if (input == 2) {
+                    firstWhile = false;
+                    Application.launch(Gui.class);
+                }
+                else {
+                    System.out.println("Invalid input. please enter 1 or 2.");
+                    scanner.nextLine();
+                }
+            }
+            else {
+                // If the next input is not an integer
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+     /*while (rmiClient.ifConnected()) {
                 // messagefromserver potrebbe essere null
                 RMIAvailability status = RMIAvailability.NOT_AVAILABLE;
                 try{
@@ -60,13 +103,4 @@ public class ClientStarter {
                 }
 
             }*/
-            }
-            else if(input==2){
-                SocketClient socket = new SocketClient(8181);
-            }
-        }
-        else if(input==2){
-            Application.launch(Gui.class);
-        }
-    }
 }
