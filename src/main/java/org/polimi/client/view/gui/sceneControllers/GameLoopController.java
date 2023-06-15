3de06d85abfa6da06e24807a9f474b5340af41b2
Filippo Garofalo <filippo.garofalo@mail.polimi.it>
2023-06-15 16:56:55 +0200

    Added Game Loop scene and controller

---
+++ src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
@@ -0,0 +1,27 @@
+package org.polimi.client.view.gui.sceneControllers;
+
+public class GameLoopController {
+    public void choosedCard(){
+
+    }
+
+    public void col0(){
+
+    }
+
+    public void col1(){
+
+    }
+
+    public void col2(){
+
+    }
+
+    public void col3(){
+
+    }
+
+    public void col4(){
+
+    }
+}
