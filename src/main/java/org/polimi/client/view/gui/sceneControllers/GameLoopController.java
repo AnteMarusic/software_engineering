18632c8bda59a1028539f0609e7ce7aa3b08c675
kala1221 <kalanakalpitha.kalupahana@mail.polimi.it>
2023-06-17 18:39:58 +0200

    ciao

--- src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
+++ src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
@@ -1,19 +1,32 @@
 package org.polimi.client.view.gui.sceneControllers;
 
 import javafx.fxml.FXML;
-
-import javax.swing.text.html.ImageView;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class GameLoopController {
     @FXML
-    private ImageView tileMatrix[];
+    private GridPane gridPane;
 
     @FXML
-    private ImageView ciao;
+    private Node ciao;
 
-
+    @FXML
     public void initialize(){
-        tileMatrix = new ImageView[9*9];
+        Image image = new Image("Gatti1_1.png");
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                ImageView imageView = new ImageView();
+                imageView.setImage(image);
+                Pane pane = new Pane();
+                pane.getChildren().add(imageView);
+                gridPane.add(pane, i, j);
+            }
+        }
     }
 
     public void choosedCard(){
