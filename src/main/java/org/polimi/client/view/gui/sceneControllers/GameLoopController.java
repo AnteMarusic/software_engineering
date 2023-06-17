3f4e15565ca26b51f866694381c826b2e76d6734
Filippo Garofalo <filippo.garofalo@mail.polimi.it>
2023-06-17 18:06:03 +0200

    new GUI scene

--- src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
+++ src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
@@ -1,6 +1,21 @@
 package org.polimi.client.view.gui.sceneControllers;
 
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ImageView;
+
 public class GameLoopController {
+    @FXML
+    private ImageView tileMatrix[];
+
+    @FXML
+    private ImageView ciao;
+
+
+    public void initialize(){
+        tileMatrix = new ImageView[9*9];
+    }
+
     public void choosedCard(){
 
     }
---
+++ src/main/java/org/polimi/client/view/gui/sceneControllers/TestSceneController.java
@@ -0,0 +1,4 @@
+package org.polimi.client.view.gui.sceneControllers;
+
+public class TestSceneController {
+}
