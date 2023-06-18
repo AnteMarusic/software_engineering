1341119ba61816de7c32965158c2bc424d34e9a9
kala1221 <kalanakalpitha.kalupahana@mail.polimi.it>
2023-06-18 21:55:38 +0200

    gui

--- src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
+++ src/main/java/org/polimi/client/view/gui/sceneControllers/GameLoopController.java
@@ -1,20 +1,31 @@
 package org.polimi.client.view.gui.sceneControllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.effect.Effect;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import org.polimi.client.ClientBoard;
+import org.polimi.client.ClientBookshelf;
 import org.polimi.servernetwork.controller.GameController;
+import org.polimi.servernetwork.model.Bookshelf;
 import org.polimi.servernetwork.model.Card;
 import org.polimi.servernetwork.model.Coordinates;
 
 import java.awt.*;
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class GameLoopController {
+    //gridpane è 450x450, ogni cella è 50x50 pixel
     @FXML
     private GridPane gridPane;
 
@@ -23,16 +34,25 @@ public class GameLoopController {
 
     @FXML
     private Image image;
+    @FXML
+    private GridPane bookshelfGridPane;
 
     private ClientBoard board;
 
+    private List<ClientBookshelf> bookshelves;
+
+    private LinkedList<Coordinates> chosenCoordinates;
+
 
     public void GameController(){
+        this.chosenCoordinates = new LinkedList<>();
+        this.bookshelves = new ArrayList<>();
     }
 
     @FXML
     public void initialize(){
         board = SceneController.getInstance().getBoard();
+        bookshelves = SceneController.getInstance().getBookshelves();
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 Card card = board.seeCardAtCoordinates(new Coordinates(i,j));
@@ -75,25 +95,34 @@ public class GameLoopController {
                         }
                         case GREEN -> {
                             switch(card.getType()){
-                                case 0 -> image = new Image("/images/17_MyShelfie_BGA/item_tiles/Gatti1.1.png");
+                                case 0 -> image = new Image("/images/17_MyShelfie_BGA/item_tiles/Gatti1_1.png");
                                 case 1 -> image = new Image("/images/17_MyShelfie_BGA/item_tiles/Gatti1.2.png");
                                 case 2 -> image = new Image("/images/17_MyShelfie_BGA/item_tiles/Gatti1.3.png");
                             }
                         }
                     }
                     ImageView imageView = new ImageView();
-                    imageView.setImage(image);
-                    imageView.setFitWidth(50);
-                    imageView.setFitHeight(50);
-                    Pane pane = new Pane();
-                    pane.getChildren().add(imageView);
-                    gridPane.add(pane, i, j);
+                    insertInGridPane(imageView, 50, 50, gridPane, i, j);
+                    imageView.setOnMouseClicked((MouseEvent event) -> {
+                        if(card.getState() == Card.State.PICKABLE){
+                            imageView.setImage(new Image("/images/17_MyShelfie_BGA/scoring_tokens/scoring_back_EMPTY.jpg"));
+                            ImageView imageView2 = new ImageView();
+                            insertInGridPane(imageView2, 25, 25, bookshelfGridPane, 0, 0);
+                        }
+                    });
                 }
 
             }
         }
     }
-
+    private void insertInGridPane(ImageView imageView, int width, int height, GridPane gridpane, int x, int y){
+        imageView.setImage(image);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+        Pane pane = new Pane();
+        pane.getChildren().add(imageView);
+        gridpane.add(pane, x, y);
+    }
 
     public void choosedCard(){
 
@@ -118,4 +147,6 @@ public class GameLoopController {
     public void col4(){
 
     }
+
+
 }
