cdffb604270aa3ed939c33418b32b94c5ec2e1a9
kala1221 <kalanakalpitha.kalupahana@mail.polimi.it>
2023-06-29 15:10:28 +0200

    tests

--- src/test/java/org/polimi/GameControllerTest.java
+++
@@ -1,7 +0,0 @@
-package org.polimi;
-
-public class GameControllerTest {
-    public static void main (String[] args) {
-        System.out.println((-1) % 4);
-    }
-}
---
+++ src/test/java/org/polimi/client/GuiClientControllerTest.java
@@ -0,0 +1,84 @@
+package org.polimi.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuiClientControllerTest {
+
+    @Test
+    void waitForFlag() {
+    }
+
+    @Test
+    void reset() {
+    }
+
+    @Test
+    void getNotified() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void handleMessage() {
+    }
+
+    @Test
+    void chooseUsername() {
+    }
+
+    @Test
+    void chooseCards() {
+    }
+
+    @Test
+    void removeOtherPlayerCards() {
+    }
+
+    @Test
+    void insertInOtherPlayerBookshelf() {
+    }
+
+    @Test
+    void alreadyTakenUsername() {
+    }
+
+    @Test
+    void chooseGameMode() {
+    }
+
+    @Test
+    void orderChosenCards() {
+    }
+
+    @Test
+    void chooseColumn() {
+    }
+
+    @Test
+    void errorMessage() {
+    }
+
+    @Test
+    void newPlayerJoinedLobby() {
+    }
+
+    @Test
+    void loginSuccessful() {
+    }
+
+    @Test
+    void reconnectionSuccessful() {
+    }
+
+    @Test
+    void modelAllMessage() {
+    }
+
+    @Test
+    void disconnect() {
+    }
+}
